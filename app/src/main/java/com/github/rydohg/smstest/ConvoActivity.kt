cccf43d2f45d2690b07155ed78d4683647936280
Ryan School <rdoh62001@gmail.com>
2017-12-06 10:45:21 -0500

    Start design of ConvoActivity

--- app/src/main/java/com/github/rydohg/smstest/ConvoActivity.kt
+++ app/src/main/java/com/github/rydohg/smstest/ConvoActivity.kt
@@ -2,13 +2,17 @@ package com.github.rydohg.smstest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 import android.util.Log
+
 
 class ConvoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_convo)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         val conversation: Conversation = intent.extras.getSerializable("convo") as Conversation
         Log.d("ConvoActivity", conversation.convoID.toString())
